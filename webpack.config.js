629e4dd5599e590ba40d443d5d4a952b4e775a28
mikinty <myou@andrew.cmu.edu>
2017-08-15 01:28:42 -0400

    introduced merging

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,53 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
 
+// webpack parts file
+const parts = require('./webpack.parts');
+
+// commonly used path variables
 const PATHS = {
   src: path.join(__dirname, 'src'),
   build: path.join(__dirname, 'build'),
 };
 
 // configuration shared by production and development
-const commonConfig = {
+const commonConfig = merge([
+  {
   // Entries have to resolve to files! They rely on Node
   // convention by default so if a directory contains *index.js*,
   // it resolves to that.
-  entry: {
-    main: PATHS.src,
+    entry: {
+      main: PATHS.src,
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Mitophace',
+      }),
+    ],
   },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Mitophace',
-    }),
-  ],
-};
+]);
 
-const productionConfig = {
+const productionConfig = merge([
 
-};
+]);
 
-const developmentConfig = {
-  devServer: {
-    // Enable history API fallback so HTML5 History API based
-    // routing works. Good for complex setups.
-    historyApiFallback: true,
-
-    // Display only errors to reduce the amount of output.
-    stats: 'errors-only',
-
-    // Parse host and port from env to allow customization.
-    host: process.env.HOST, // Defaults to `localhost`
-    port: process.env.PORT, // Defaults to 8080
-  },
-};
+const developmentConfig = merge([
+  parts.devServer({
+    // customize host/port here if necessary
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
 
 module.exports = (env) => {
-    console.log('env', env);
+  if (env === 'production') {
+    return merge(commonConfig, productionConfig);
+  }
 
-    return commonConfig;
-}
+  return merge(commonConfig, developmentConfig);
+};
