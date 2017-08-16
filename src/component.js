de0c51d9148d29f31241f6a6364e7e6b36ab10bc
mikinty <myou@andrew.cmu.edu>
2017-08-16 02:30:42 -0400

    demonstrated code splitting

--- src/component.js
+++ src/component.js
@@ -7,5 +7,13 @@ export default (text = 'Setup') => {
 
   element.className = styles.redButton;
 
+  element.onclick = () => {
+    import('./lazy').then((lazy) => {
+      element.textContent = lazy.default;
+    }).catch((err) => {
+      console.error(err);
+    });
+  };
+
   return element;
 };
