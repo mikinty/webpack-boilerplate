085afa7752464d33efc08b8ddc88e61dd80b5f9b
mikinty <myou@andrew.cmu.edu>
2017-08-17 23:31:34 -0400

    switched back to es6 for tests since added that support, also added code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,9 @@
+const path = require('path');
+
 module.exports = (config) => {
   const tests = 'tests/*.test.js';
+
+  process.env.BABEL_ENV = 'karma';  
 
   config.set({
     frameworks: ['mocha'],
@@ -18,5 +22,19 @@ module.exports = (config) => {
     singleRun: true,
 
     browsers: ['PhantomJS'],    
+
+    webpack: require('./webpack.parts').loadJavaScript({
+      include: path.join(__dirname, 'tests'),
+    }),
+
+    reporters: ['coverage'],
+
+    coverageReporter: {
+      dir: 'build',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcov' },
+      ],
+    },
   });
 };
